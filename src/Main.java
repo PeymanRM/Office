125e6b5125d49269f2a4a7d9d75a5118c976bd3c
Peyman RM <PRM.8193@gmail.com>
2022-05-18 15:51:35 +0430

    created Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        //TODO load an fxml file
+        Parent root = FXMLLoader.load(getClass().getResource("views/Login.fxml"));
+        Scene scene = new Scene(root);
+        String css = this.getClass().getResource("./views/css/styles.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.setTitle("Office");
+        stage.setResizable(false);
+        stage.show();
     }
 
     public static void main(String[] args) {
