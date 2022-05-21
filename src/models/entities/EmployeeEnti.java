3ac948ce8f2971229d6f93e486ad17f8c3f7b3ce
Kasrabakht <kasrabakht30@gmail.com>
2022-05-21 23:03:35 +0430

    Ninth Commit

---
+++ src/models/entities/EmployeeEnti.java
@@ -0,0 +1,28 @@
+package models.entities;
+
+public class EmployeeEnti {
+    private int id;private String name;private String fatherName;private int age;private String address;private String phone;private String landLine;
+    private String degree;private int deptId;private String position;private int salary;
+    public int getId() {return id;}
+    public EmployeeEnti setId(int id) {this.id = id;return this;}
+    public String getName() {return name;}
+    public EmployeeEnti setName(String name) {this.name = name;return this;}
+    public String getFatherName() {return fatherName;}
+    public EmployeeEnti setFatherName(String fatherName) {this.fatherName = fatherName;return this;}
+    public int getAge() {return age;}
+    public EmployeeEnti setAge(int age) {this.age = age;return this;}
+    public String getAddress() {return address;}
+    public EmployeeEnti setAddress(String address) {this.address = address;return this;}
+    public String getPhone() {return phone;}
+    public EmployeeEnti setPhone(String phone) {this.phone = phone;return this;}
+    public String getLandLine() {return landLine;}
+    public EmployeeEnti setLandLine(String landLine) {this.landLine = landLine;return this;}
+    public String getDegree() {return degree;}
+    public EmployeeEnti setDegree(String degree) {this.degree = degree;return this;}
+    public int getDeptId() {return deptId;}
+    public EmployeeEnti setDeptId(int deptId) {this.deptId = deptId;return this;}
+    public String getPosition() {return position;}
+    public EmployeeEnti setPosition(String position) {this.position = position;return this;}
+    public int getSalary() {return salary;}
+    public EmployeeEnti setSalary(int salary) {this.salary = salary;return this;}
+}
