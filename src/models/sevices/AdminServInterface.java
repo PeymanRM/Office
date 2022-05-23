6fbe2878a84b3583afad3fbb567e977f99298f16
Peyman RM <PRM.8193@gmail.com>
2022-05-24 00:16:53 +0430

    created service interface for admin and employee

---
+++ src/models/sevices/AdminServInterface.java
@@ -0,0 +1,13 @@
+package models.sevices;
+
+public interface AdminServInterface {
+
+    /**
+     * Gets id of an admin and checks if entered password
+     * and the actual password of the admins match
+     * @param adminId the entered id in the login menu
+     * @param enteredPassword the entered password in the login menu
+     * @return true, if passwords match and false if they don't
+     */
+    boolean verifyPassword(String adminId, String enteredPassword);
+}
