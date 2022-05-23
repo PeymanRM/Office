8c0d001e27a9180d59a03560b1fdf2c9772fa6a9
Peyman RM <PRM.8193@gmail.com>
2022-05-23 20:30:52 +0430

    added main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("views/Login.fxml"));
+        Scene scene = new Scene(root);
+        String css = this.getClass().getResource("./views/css/styles.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.setTitle("Office");
+        stage.setResizable(false);
+        //TODO add icon
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
