42bce7bc0540961f6dae6509fbff055a526ed203
Peyman RM <PRM.8193@gmail.com>
2022-05-27 21:50:59 +0430

    completed EmployeeCreateController

---
+++ src/controllers/EmployeeCreateController.java
@@ -0,0 +1,68 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import models.entities.EmployeeEnti;
+import validators.EmployeeInputException;
+import validators.EmployeeValidator;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class EmployeeCreateController {
+
+    @FXML
+    private TextField nameTextField, fatherNameTextField, ageTextField, addressTextField, degreeTextField,
+            landlineTextField, phoneNumberTextField, departmentIdTextField, positionTextField, salaryTextField;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    private String employeeId;
+
+    public void add(ActionEvent event) {
+        try{
+            //validation
+            EmployeeValidator employee = new EmployeeValidator();
+            employee.setName(nameTextField.getText().trim()).setFatherName(fatherNameTextField.getText().trim())
+                    .setAddress(addressTextField.getText().trim()).setDegree(degreeTextField.getText().trim())
+                    .setLandLine(landlineTextField.getText().trim()).setPhone(phoneNumberTextField.getText().trim())
+                    .setDeptId(departmentIdTextField.getText().trim()).setPosition(positionTextField.getText().trim());
+            employee.setSAge(ageTextField.getText().trim()).setSSalary(salaryTextField.getText().trim());
+            employee.validateInputs();
+            employee.setVerifiedIntVariables();
+
+            //save
+            EmployeeServ.getInstance().saveEmployee(employee);
+
+            //switch scene
+            FXMLLoader loader = new FXMLLoader (getClass().getResource("../views/Main_View.fxml"));
+            root = loader.load();
+            MainMenuController mainMenuController = loader.getController();
+            //TODO set status of edit in main menu
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (EmployeeInputException e){
+            System.out.println("input error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
+    }
+
+    public void cancel(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../views/Main_View.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
