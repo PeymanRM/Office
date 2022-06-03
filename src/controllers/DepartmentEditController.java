83045bc36466265614cc7329e8182f312a0e0f67
Peyman RM <PRM.8193@gmail.com>
2022-06-04 00:46:37 +0430

    implemented DepartmentEditController

---
+++ src/controllers/DepartmentEditController.java
@@ -0,0 +1,79 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import models.entities.DepartmentEnti;
+import models.services.DepartmentServ;
+import validators.DepartmentInputException;
+import validators.DepartmentValidator;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class DepartmentEditController {
+
+    @FXML
+    private TextField nameTextField, dutiesTextField;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    private DepartmentEnti preDepartment;
+
+    public void setDepartment(String departmentId) {
+        try {
+            this.preDepartment = DepartmentServ.getInstance().getDepartmentInfo(departmentId);
+            DepartmentEnti department = DepartmentServ.getInstance().getDepartmentInfo(departmentId);
+            nameTextField.setText(department.getName());
+            dutiesTextField.setText(department.getDuties());
+        } catch (SQLException e){
+            System.out.println("initialize SQL error: " + e.getMessage());
+        } catch(Exception e){
+            System.out.println("initialize error: " + e.getMessage());
+        }
+    }
+
+    public void edit(ActionEvent event) throws IOException {
+        try{
+            //validation
+            DepartmentValidator department = new DepartmentValidator();
+            department.setName(nameTextField.getText().trim()).setDuties(dutiesTextField.getText().trim());
+            department.validateInputs();
+
+            //save
+            preDepartment.setName(nameTextField.getText().trim()).setDuties(dutiesTextField.getText().trim());
+            DepartmentServ.getInstance().editDepartment(preDepartment);
+
+            //switch scene
+            FXMLLoader loader = new FXMLLoader (getClass().getResource("../views/Main_View.fxml"));
+            root = loader.load();
+            MainMenuController mainMenuController = loader.getController();
+            //TODO set status of edit in main menu
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (DepartmentInputException e){
+            System.out.println("input error: " + e.getMessage());
+        } catch (SQLException e){
+            System.out.println("sql error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
+    }
+
+    public void cancel(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../views/Main_View.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
