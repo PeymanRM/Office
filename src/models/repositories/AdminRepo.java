cddce8a50c954dd5fd1e9801d0285b9292a32a10
Kasrabakht <kasrabakht30@gmail.com>
2022-06-03 22:15:36 +0430

    Admin Update

---
+++ src/models/repositories/AdminRepo.java
@@ -0,0 +1,46 @@
+package models.repositories;
+import java.sql.*;
+import models.entities.AdminEnti;
+import models.sevices.AdminServInterface;
+public class AdminRepo implements AdminServInterface {
+    boolean swtch;
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+
+    public AdminRepo() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/office", "JavaLearner", "LearningIsFun,HomeworkIsn't");
+        connection.setAutoCommit(false);
+    }
+
+    @Override
+    public boolean verifyAdmin(String adminId, String enteredPassword) throws SQLException {
+        AdminEnti admin = new AdminEnti();
+        preparedStatement = connection.prepareStatement("select * from admins where username=?");
+        preparedStatement.setString(1, adminId);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            admin.setUsername(((ResultSet) resultSet).getString("username"));
+            admin.setPassword(resultSet.getString("password"));
+        }
+        if (adminId.equals(admin.getUsername()) && enteredPassword.equals(admin.getPassword())) {
+            swtch = true;
+        } else {
+            swtch = false;
+        }
+        return swtch;
+    }
+
+    @Override
+    public void addFirstAdmin() throws SQLException {
+        preparedStatement = connection.prepareStatement("insert into admins(username,password) values (?,?)");
+        preparedStatement.setString(1, "peyman");
+        preparedStatement.setString(2, "peyman123");
+        preparedStatement.executeUpdate();
+
+
+    }
+
+    public void Commit() throws SQLException {
+        connection.commit();
+    }
+}
--- src/models/sevices/AdminServ.java
+++ src/models/sevices/AdminServ.java
@@ -1,21 +1,28 @@
 package models.sevices;
 
+import models.repositories.AdminRepo;
 import java.sql.SQLException;
+public class AdminServ implements AdminServInterface {
+    private AdminServ() {
+    }
 
-public class AdminServ implements AdminServInterface{
-    private AdminServ() {}
     private static AdminServ adminServ = new AdminServ();
-    public static AdminServ getInstance(){
+
+    public static AdminServ getInstance() {
         return adminServ;
     }
 
     @Override
     public boolean verifyAdmin(String adminId, String enteredPassword) throws SQLException {
-        return true;
+        AdminRepo adminRepo = new AdminRepo();
+        return adminRepo.verifyAdmin(adminId, enteredPassword);
     }
 
     @Override
-    public void addFirstAdmin() throws SQLException{
+    public void addFirstAdmin() throws SQLException {
+        AdminRepo adminRepo = new AdminRepo();
+        adminRepo.addFirstAdmin();
+        adminRepo.Commit();
 
     }
 }
