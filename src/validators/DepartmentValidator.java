2873eadd3faf28463f647a2d8e9b84762d091721
Peyman RM <PRM.8193@gmail.com>
2022-06-05 11:08:03 +0430

    refactored validators

--- src/validators/DepartmentValidator.java
+++ src/validators/DepartmentValidator.java
@@ -8,6 +8,7 @@ public class DepartmentValidator extends DepartmentEnti {
 
     public void validateInputs() throws Exception {
         //Name
+        if (getName().length() == 0) throw new DepartmentInputException("Enter a name!");
         if (getName().length() < 3) throw new DepartmentInputException("Entered name is too short!");
         if (!Pattern.matches("^[a-zA-Z ,.'-]+$", getName())) throw new DepartmentInputException("Invalid name!");
     }
